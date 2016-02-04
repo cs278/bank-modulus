6e836bfc4277954dd584888ab816823f544713b7
Chris Smith <chris@cs278.org>
2016-02-04 21:44:19 +0000

    Stub files

---
+++ src/BankModulus.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Cs278\BankModulus;
+
+final class BankModulus
+{
+    public function check($sortCode, $accountNumber)
+    {
+        return false;
+    }
+}
---
+++ src/Exception/SortCodeUnknownException.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Cs278\BankModulus\Exception;
+
+final class SortCodeUnknownException extends \Exception implements Exception
+{
+}
