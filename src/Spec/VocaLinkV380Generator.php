be2c6334af409c354b3a0faa4f084c6c3fede90a
Chris Smith <chris@cs278.org>
2016-02-05 23:30:32 +0000

    Add optimised generation mode

--- src/Spec/VocaLinkV380Generator.php
+++ src/Spec/VocaLinkV380Generator.php
@@ -4,7 +4,7 @@ namespace Cs278\BankModulus\Spec;
 
 final class VocaLinkV380Generator
 {
-    public static function generate($input, $output)
+    public static function generate($input, $output, $optimise)
     {
         $inHandle = is_resource($input) ? $input : fopen($input, 'r');
         $outHandle = is_resource($output) ? $output : fopen($output, 'x');
@@ -18,13 +18,78 @@ final class VocaLinkV380Generator
         fwrite($outHandle, $tab.'/'.'** @internal */'."\n");
         fwrite($outHandle, $tab.'final protected function fetchRecord($sortCode, $pass)'."\n$tab{\n");
 
-        self::generateSimple($inHandle, $outHandle);
+        if ($optimise) {
+            self::generateOptimised($inHandle, $outHandle);
+        } else {
+            self::generateSimple($inHandle, $outHandle);
+        }
 
         fwrite($outHandle, "$tab}\n"); // Close function
         fwrite($outHandle, "}\n"); // Close class
 
         fclose($inHandle);
         fclose($outHandle);
+    }
+
+    private static function generateOptimised($inHandle, $outHandle)
+    {
+        $tab = '        ';
+
+        $seen = [];
+        $uniqueWeights = [];
+        $sortCodes = [];
+
+        // Initial pass over data file, organise the unqiue return values and the
+        // conditions required to hit them.
+        while ($line = trim(fgets($inHandle))) {
+            $cols = preg_split('{\s+}', $line);
+
+            $sortCodeStart = $cols[0];
+            $sortCodeEnd = $cols[1];
+            $algorithm = strtoupper($cols[2]);
+            $weights = array_map('intval', array_slice($cols, 3, 6 + 8));
+            $exception = isset($cols[17]) ? (int) $cols[17] : 0;
+            $cols = null;
+
+            $digest = hash('sha256', serialize([$algorithm, $exception, $weights]));
+
+            if (!isset($uniqueWeights[$digest])) {
+                $uniqueWeights[$digest] = [$algorithm, $exception, $weights];
+                $sortCodes[$digest] = [];
+            }
+
+            if (!isset($seen[$sortCodeStart.$sortCodeEnd])) {
+                $seen[$sortCodeStart.$sortCodeEnd] = 1;
+            }
+
+            $pass = $seen[$sortCodeStart.$sortCodeEnd]++;
+
+            $sortCodes[$digest][] = [$pass, $sortCodeStart, $sortCodeEnd];
+        }
+
+        // Write out file content with a single if statement for each unique
+        // return value.
+        foreach ($uniqueWeights as $digest => $weight) {
+            fwrite($outHandle, "{$tab}if (\n");
+
+            foreach ($sortCodes[$digest] as $i => $row) {
+                $combiner = ($i === 0) ? '   ' : '|| ';
+
+                fwrite($outHandle, "$tab    $combiner");
+                fwrite($outHandle, self::getComparison($row[0], $row[1], $row[2]));
+                fwrite($outHandle, "\n");
+            }
+
+            fwrite($outHandle, "{$tab}) {\n");
+            fwrite($outHandle, sprintf(
+                "$tab    return [%s, [%s], %u];\n",
+                var_export($weight[0], true),
+                implode(', ', $weight[2]),
+                $weight[1]
+            ));
+            fwrite($outHandle, "{$tab}}\n\n");
+        }
+        fseek($outHandle, ftell($outHandle) - 1); // Remove a \n
     }
 
     private static function generateSimple($inHandle, $outHandle)
@@ -91,5 +156,5 @@ final class VocaLinkV380Generator
 
 // Violation of PSR1/2 but it's a dev file so sue me.
 if ('cli' === PHP_SAPI && isset($_SERVER['PHP_SELF']) && __FILE__ === realpath($_SERVER['PHP_SELF'])) {
-    VocaLinkV380Generator::generate(STDIN, STDOUT);
+    VocaLinkV380Generator::generate(STDIN, STDOUT, isset($argv[1]) && $argv[1] === '--optimise');
 }
