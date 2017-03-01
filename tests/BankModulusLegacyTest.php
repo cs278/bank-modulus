c5f85a2492d05f8c12a374045ec64b3a113ab705
Chris Smith <chris@cs278.org>
2017-03-01 21:36:41 +0000

    Update tests to not cause deprecation notices

--- tests/BankModulusLegacyTest.php
+++ tests/BankModulusLegacyTest.php
@@ -2,61 +2,20 @@
 
 namespace Cs278\BankModulus;
 
-use Cs278\BankModulus\Spec\SimpleSpecFactory;
-
 /**
+ * Snapshot of BankModulus tests that pass in SpecInterface directly.
+ *
  * @covers Cs278\BankModulus\BankModulus
+ * @group legacy
  */
-final class BankModulusTest extends \PHPUnit_Framework_TestCase
+final class BankModulusLegacyTest extends \PHPUnit_Framework_TestCase
 {
-    public function testConstructorNoArgs()
-    {
-        $modulus = new BankModulus();
-
-        $this->assertInstanceOf('Cs278\\BankModulus\\BankModulus', $modulus);
-    }
-
-    public function testConstructorInvalidSpec()
-    {
-        $this->setExpectedException(
-            'Cs278\\BankModulus\\Exception\\InvalidArgumentException',
-            sprintf(
-                'Expected an instance of %1$s\\SpecFactoryInterface, %1$s\\SpecInterface or NULL. Got: stdClass',
-                'Cs278\\BankModulus\\Spec'
-            )
-        );
-
-        $modulus = new BankModulus(new \stdClass());
-    }
-
-    /**
-     * Tests the backwards compatability layer in the constructor.
-     *
-     * @requires function error_clear_last
-     * @group legacy
-     */
-    public function testConstructorConcreteSpec()
-    {
-        error_clear_last();
-
-        $modulus = new BankModulus(new Mock\SpecPass());
-        $error = error_get_last();
-
-        $this->assertInstanceOf('Cs278\\BankModulus\\BankModulus', $modulus);
-        $this->assertArraySubset([
-            'message' => 'Passing an instance of SpecInterface to Cs278\\BankModulus\\BankModulus::__construct() is deprecated and will be removed in version 2.0.0.',
-            'type' => E_USER_DEPRECATED,
-        ], $error);
-
-        error_clear_last();
-    }
-
     public function testCheckValid()
     {
         $spec = new Mock\SpecPass();
         $normalizer = new Mock\Normalizer();
 
-        $modulus = new BankModulus(new SimpleSpecFactory($spec), $normalizer);
+        $modulus = new BankModulus($spec, $normalizer);
         $this->assertTrue($modulus->check('089999', '66374958'));
     }
 
@@ -65,7 +24,7 @@ final class BankModulusTest extends \PHPUnit_Framework_TestCase
         $spec = new Mock\SpecFail();
         $normalizer = new Mock\Normalizer();
 
-        $modulus = new BankModulus(new SimpleSpecFactory($spec), $normalizer);
+        $modulus = new BankModulus($spec, $normalizer);
         $this->assertFalse($modulus->check('089999', '66374959'));
     }
 
@@ -74,7 +33,7 @@ final class BankModulusTest extends \PHPUnit_Framework_TestCase
         $spec = new Mock\SpecUnknown();
         $normalizer = new Mock\Normalizer();
 
-        $modulus = new BankModulus(new SimpleSpecFactory($spec), $normalizer);
+        $modulus = new BankModulus($spec, $normalizer);
         $this->assertTrue($modulus->check('000000', '12345678'));
     }
 
@@ -83,7 +42,7 @@ final class BankModulusTest extends \PHPUnit_Framework_TestCase
         $spec = new Mock\SpecPass();
         $normalizer = new Mock\NormalizerReverse();
 
-        $modulus = new BankModulus(new SimpleSpecFactory($spec), $normalizer);
+        $modulus = new BankModulus($spec, $normalizer);
 
         $result = $modulus->lookup('12-34-56', '12345678');
 
@@ -101,7 +60,7 @@ final class BankModulusTest extends \PHPUnit_Framework_TestCase
         $spec = new Mock\SpecFail();
         $normalizer = new Mock\NormalizerReverse();
 
-        $modulus = new BankModulus(new SimpleSpecFactory($spec), $normalizer);
+        $modulus = new BankModulus($spec, $normalizer);
 
         $result = $modulus->lookup('12-34-56', '12345678');
 
@@ -119,7 +78,7 @@ final class BankModulusTest extends \PHPUnit_Framework_TestCase
         $spec = new Mock\SpecUnknown();
         $normalizer = new Mock\NormalizerReverse();
 
-        $modulus = new BankModulus(new SimpleSpecFactory($spec), $normalizer);
+        $modulus = new BankModulus($spec, $normalizer);
 
         $result = $modulus->lookup('12-34-56', '12345678');
 
@@ -137,7 +96,7 @@ final class BankModulusTest extends \PHPUnit_Framework_TestCase
         $spec = new Mock\SpecPass();
         $normalizer = new Mock\NormalizerUnsupported();
 
-        $modulus = new BankModulus(new SimpleSpecFactory($spec), $normalizer);
+        $modulus = new BankModulus($spec, $normalizer);
 
         $result = $modulus->lookup('12-34-56', '12345678');
 
@@ -150,51 +109,12 @@ final class BankModulusTest extends \PHPUnit_Framework_TestCase
         $this->assertTrue($result->isValid());
     }
 
-    /** @dataProvider dataNormalize */
-    public function testNormalize($expectedSortCode, $expectedAccountNumber, $sortCode, $accountNumber)
-    {
-        $modulus = new BankModulus();
-
-        $this->assertNull($modulus->normalize($sortCode, $accountNumber));
-
-        $this->assertSame($expectedSortCode, $sortCode);
-        $this->assertSame($expectedAccountNumber, $accountNumber);
-    }
-
-    public function dataNormalize()
-    {
-        return [
-            // Formatting
-            ['123456', '12345678', '12-34-56', '1-2345678'],
-            ['123456', '02345678', '12 34 56', '     2345678'],
-
-            // Co-op Bank
-            ['081245', '12345678', '081245', '1234567890'],
-            ['081245', '00123456', '081245', '0012345678'],
-            // NatWest
-            ['600000', '23456789', '600000', '01-23456789'],
-            ['600000', '23456789', '600000', '0123456789'],
-            // Santander
-            ['091231', '23456789', '091234', '123456789'],
-            ['091237', '55555555', '091234', '755555555'],
-            ['724321', '23456789', '724321', '123456789'],
-            // Seven Digit
-            ['123456', '01234567', '123456', '1234567'],
-            ['123456', '00000000', '123456', '0000000'],
-            ['123456', '09999999', '123456', '9999999'],
-            // Six Digit
-            ['123456', '00123456', '123456', '123456'],
-            ['123456', '00000000', '123456', '000000'],
-            ['123456', '00999999', '123456', '999999'],
-        ];
-    }
-
     public function testNormalizeUnsupported()
     {
         $spec = new Mock\SpecPass();
         $normalizer = new Mock\NormalizerUnsupported();
 
-        $modulus = new BankModulus(new SimpleSpecFactory($spec), $normalizer);
+        $modulus = new BankModulus($spec, $normalizer);
 
         $sortCode = '0 00000';
         $accountNumber = '0 0000000';
@@ -213,7 +133,7 @@ final class BankModulusTest extends \PHPUnit_Framework_TestCase
         $spec = $this->getMockForAbstractClass('Cs278\BankModulus\Spec\SpecInterface');
         $normalizer = $this->getMockForAbstractClass('Cs278\BankModulus\BankAccountNormalizer\NormalizerInterface');
 
-        $modulus = new BankModulus(new SimpleSpecFactory($spec), $normalizer);
+        $modulus = new BankModulus($spec, $normalizer);
 
         foreach (['lookup', 'normalize', 'check'] as $method) {
             unset($sortCode, $accountNumber);
