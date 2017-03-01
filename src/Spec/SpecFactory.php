2ecad94a5159925299141d8f81acf36abbc76243
Chris Smith <chris@cs278.org>
2017-03-01 20:47:24 +0000

    Add default factory implementation

---
+++ src/Spec/SpecFactory.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Cs278\BankModulus\Spec;
+
+/**
+ * Default factory implementation.
+ *
+ * Uses the correct specification based on the current date.
+ */
+final class SpecFactory implements SpecFactoryInterface
+{
+    /** @var \DateTime|null */
+    private $now;
+
+    /** {@inheritdoc} */
+    public function create()
+    {
+        $this->now = new \DateTime('today');
+
+        if ($this->dateOnOrAfter('2017-01-09')) {
+            return new VocaLinkV400();
+        }
+
+        return new VocaLink390();
+    }
+
+    private function dateOnOrAfter($when)
+    {
+        Assert::string($when);
+        Assert::regex($when, '{^[0-9]{4}-[0-9]{2}-[0-9]{2}$}');
+
+        $when = \DateTime::createFromFormat('!Y-m-d', $when);
+
+        assert($when instanceof \DateTime);
+
+        return $this->now >= $when;
+    }
+}
