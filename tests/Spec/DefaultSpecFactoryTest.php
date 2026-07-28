163202a64ea650c3fae5c62b54d8ea3a69f9ca4e
Chris Smith <chris.smith@widerplan.com>
2026-07-28 11:01:45 +0100

    Import specification

--- tests/Spec/DefaultSpecFactoryTest.php
+++ tests/Spec/DefaultSpecFactoryTest.php
@@ -124,7 +124,8 @@ final class DefaultSpecFactoryTest extends \PHPUnit\Framework\TestCase
             ['VocaLinkV850', new \DateTime('2025-07-26')],
             ['VocaLinkV870', new \DateTime('2026-03-28')],
             ['VocaLinkV890', new \DateTime('2026-06-20')],
-            ['VocaLinkV890', new \DateTime('2030-01-01')],
+            ['VocaLinkV900', new \DateTime('2026-08-15')],
+            ['VocaLinkV900', new \DateTime('2099-01-01')],
         ];
     }
 
